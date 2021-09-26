4e9b70de380c61b2393f9d50bfe1052857ef804b
lya <thedenike@gmail.com>
2021-09-26 19:53:25 +0300

    asd

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    
+
+    class Items {
+        constructor(name,photo,price){
+            this.name = name
+            this.photo = photo
+            this.price = price
+        }
+        createElement(){
+            const productlist = document.querySelector('.productslist')
+            const item = document.createElement('div')
+            item.classList.add('product-item')
+            item.innerHTML = 
+                `
+                <h1 class="item_name">${this.name}</h1>
+                <img class="item_img" src="${this.photo}" alt="">
+                <p class="item-price">${this.price}</p>
+
+                `
+            productlist.append(item)
+        }
+    }
+
+    const firstItem = new Items('Air force 1 black', 'assets/blackforce.jfif','120$')
+    const secondItem = new Items('Air force 1 pink', 'assets/pinklforce.jfif','130$')
+    const thirdItem = new Items('Air force 1 pink', 'assets/whiteanime.jfif','140$')
+    const fourthItem = new Items('Air force 1 white', 'assets/whitecustom.jfif','150$')
+    const fivesItem = new Items('Air force 1  white', 'assets/whiteforce.jfif','100$')
+    const sixItem = new Items('Air force 1 w/b', 'assets/whitewithblack.jfif','170$')
+
+    firstItem.createElement()
+    secondItem.createElement()
+    thirdItem.createElement()
+    fourthItem.createElement()
+    fivesItem.createElement()
+    sixItem.createElement()
+
+})  
